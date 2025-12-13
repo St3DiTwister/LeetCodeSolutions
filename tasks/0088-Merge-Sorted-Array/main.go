5b13f6e1f8b4f513ffcd9cfec40b52ea124ae79d
st3ditwister <krasnov00200@bk.ru>
2025-12-13 23:20:18 +0300

    Add solutions and tests for: - 88 Merge Sorted Array

---
+++ tasks/0088-Merge-Sorted-Array/main.go
@@ -0,0 +1,17 @@
+package _088_Merge_Sorted_Array
+
+func merge(nums1 []int, m int, nums2 []int, n int) []int {
+	i, j, k := m-1, n-1, m+n-1
+
+	for j >= 0 {
+		if i >= 0 && nums1[i] > nums2[j] {
+			nums1[k] = nums1[i]
+			i--
+		} else {
+			nums1[k] = nums2[j]
+			j--
+		}
+		k--
+	}
+	return nums1
+}
